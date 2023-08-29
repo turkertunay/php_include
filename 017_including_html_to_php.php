bf67ace8cde9f5cab19f5792105219e25caa87af
turkertunay <75833839+turkertunay@users.noreply.github.com>
2023-08-29 21:01:49 +0300

    Add files via upload

---
+++ 017_including_html_to_php.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  <?php include "header.html"; ?>
+  <p>lorem ipsum... </p>
+  <?php include "footer.html"; ?>
+
+
+
+
+  </body>
+</html>
